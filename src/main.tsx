ea5f5ed577e353b463776ee3dec5b03705ac11d6
Claire Kwon <0304gkdud@gmail.com>
2026-04-09 19:26:20 -0700

    feat: Sentry error monitoring

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,18 @@
 import { createRoot } from "react-dom/client";
+import * as Sentry from "@sentry/react";
 import App from "./App.tsx";
 import "./index.css";
+
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  environment: import.meta.env.MODE,
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  tracesSampleRate: 0.2,
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
 
 createRoot(document.getElementById("root")!).render(<App />);
